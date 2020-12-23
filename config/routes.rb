bc8b7c3e55c5d283ff7ed8801f5d981003665a01
Santiago J. Alba <santij.alba@gmail.com>
2020-12-23 07:01:39 +0100

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tasks do
-    member do
-      patch :complete
-    end
-  end
   root 'tasks#index'
+  resources :users do
+    resources :tasks do
+      member do
+        patch :complete
+      end
+    end
+    resources :tags
+  end
 end
